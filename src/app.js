eb5cf9427af1d2ba4bc014b8a4174ee96aaab555
agusrabadan <158159060+agusrabadan@users.noreply.github.com>
2024-02-27 11:04:35 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#Excusa").innerHTML = generateExcuse();
+};
+console.log();
+
+let generateExcuse = () => {
+  let quien = ["Mi madre", "Mi tía", "El gato", "El profesor"];
+  let accion = ["rompió", "utilizó", "quemó", "perdió"];
+  let cosa = ["los libros", "la comida", "la casa", "el lavabo"];
+  let cuando = ["ayer.", "la semana pasada.", "hace un rato.", "hoy."];
+
+  let quienIndx = Math.floor(Math.random() * quien.length);
+  let accionIndx = Math.floor(Math.random() * accion.length);
+  let cosaIndx = Math.floor(Math.random() * cosa.length);
+  let cuandoIndx = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[quienIndx] +
+    " " +
+    accion[accionIndx] +
+    " " +
+    cosa[cosaIndx] +
+    " " +
+    cuando[cuandoIndx]
+  );
 };
